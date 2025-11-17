4a1d27ab82c840635ba59184825334470a3e4cf1
Yessi781 <yessirodriguez579@gmail.com>
2025-11-17 10:53:55 -0500

    Delete ConversorDeMoneda directory

--- ConversorDeMoneda/src/main/java/com/conversor/Main.java
+++
@@ -1,9 +0,0 @@
-package com.conversor;
-
-import com.conversor.ui.Menu;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new Menu().mostrar();
-    }
-}
